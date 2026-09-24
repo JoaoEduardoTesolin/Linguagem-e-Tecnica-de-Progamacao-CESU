4207a642223bf44e79489420037e1e94169eab62
Joao Eduardo <jecarnelosi2@gmail.com>
2026-09-23 21:25:02 -0300

    maiorentre3numeros

---
+++ maior entre 3 numeros.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int numero1, numero2, numero3;
+	
+	printf("Entre com o valor de a\n");
+	printf("Entre com o valor de b\n");
+	printf("Entre com o valor de c\n");
+	
+	scanf("%d", &numero1);
+	scanf("%d", &numero2);
+	scanf("%d", &numero3);
+	
+	if( numero1 > numero2 && numero1 > numero3){
+		printf("o maior eh A");
+	}
+	
+	if(numero2 > numero1 && numero2 > numero3){
+		printf("o maior eh B");
+	}
+	
+	if (numero3 > numero1 && numero3 > numero2){
+		printf("o maior eh C");
+	}
+	return 0;
+}
